eaeb4e42191cd85eae29b9acf489944ffd90efaa
Sayad Mohammad Mahadi Hassan <71854514+mahadi137@users.noreply.github.com>
2021-03-03 22:22:37 +0200

    Add files via upload

---
+++ Assignment 3/Frequency_2GParall_2019.gf46/Frequency_2GParall_2019.30257.bat
@@ -0,0 +1,4 @@
+call "C:\Program Files (x86)\GFortran\4.6\bin\gf46vars.bat"
+pushd "C:\Users\mahadi\Desktop\Electric Power System\40% Assignments\A3\Frequency_2GParall_2019.gf46\"
+Frequency_2GParall_2019.exe -v4 192.168.0.3 30257 locale=english-us
+popd
---
+++ Assignment 3/Frequency_BaseCase_2019.gf46/Frequency_BaseCase_2019.mak.bat
@@ -0,0 +1,4 @@
+call "C:\Program Files (x86)\GFortran\4.6\bin\gf46vars.bat"
+pushd "C:\Users\mahadi\Desktop\Electric Power System\40% Assignments\A3\Frequency_BaseCase_2019.gf46\"
+make -f Frequency_BaseCase_2019.mak
+popd
